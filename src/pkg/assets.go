740306ad6aec06ef4b8da7472315d9928933ec1b
Luuan11 <luancraft55@gmail.com>
2025-03-25 19:19:00 -0300

    :sparkles: changing project and adding assets

---
+++ src/pkg/assets.go
@@ -0,0 +1,88 @@
+package assets
+
+import (
+	"embed"
+	"fmt"
+	"image"
+	_ "image/png"
+	"io/fs"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"golang.org/x/image/font"
+	"golang.org/x/image/font/opentype"
+)
+
+//go:embed *
+var assets embed.FS
+
+var PlayerSprite = mustLoadImage("profile/player.png")
+var LaserSprite = mustLoadImage("profile/laser.png")
+var GopherPlayer = mustLoadImage("profile/go_player.png")
+
+var MeteorSprites = mustLoadImages("meteors/*.png")
+var StarsSprites = mustLoadImages("stars/*.png")
+var PlanetsSprites = mustLoadImages("planets/*.png")
+
+var ScoreFont = mustLoadFont("font/font.ttf")
+var FontUi = mustLoadFont("font/fontui.ttf")
+
+var PowerUpSprites = mustLoadImage("powers/powerups.png")
+var SuperPowerSprite = mustLoadImage("powers/superpower.png")
+
+func mustLoadImage(name string) *ebiten.Image {
+	f, err := assets.Open(name)
+	if err != nil {
+		fmt.Println("Error loading image", err)
+		panic(err)
+	}
+
+	defer f.Close()
+
+	img, _, err := image.Decode(f)
+	if err != nil {
+		fmt.Println("Error loading image", err)
+		panic(err)
+	}
+
+	return ebiten.NewImageFromImage(img)
+}
+
+func mustLoadImages(path string) []*ebiten.Image {
+	matches, err := fs.Glob(assets, path)
+	if err != nil {
+		fmt.Println("Error loading images", err)
+		panic(err)
+	}
+
+	images := make([]*ebiten.Image, len(matches))
+	for i, match := range matches {
+		images[i] = mustLoadImage(match)
+	}
+
+	return images
+}
+
+func mustLoadFont(name string) font.Face {
+	f, err := assets.ReadFile(name)
+	if err != nil {
+		fmt.Println("Error loading font", err)
+		panic(err)
+	}
+
+	tt, err := opentype.Parse(f)
+	if err != nil {
+		panic(err)
+	}
+
+	face, err := opentype.NewFace(tt, &opentype.FaceOptions{
+		Size:    48,
+		DPI:     72,
+		Hinting: font.HintingVertical,
+	})
+	if err != nil {
+		fmt.Println("Error loading font", err)
+		panic(err)
+	}
+
+	return face
+}
