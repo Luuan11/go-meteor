682f4cbfcd99fbf90754acec89272921b28b6e09
Luuan11 <luancraft55@gmail.com>
2025-11-11 21:44:43 -0300

    :sparkles: feat: adding logs for debbuging on desktop

--- internal/core/game_wasm.go
+++ internal/core/game_wasm.go
@@ -9,9 +9,15 @@ import (
 )
 
 func (g *Game) notifyWebLeaderboard(name string, score int) {
+	js.Global().Get("console").Call("log", "[GO] notifyWebLeaderboard called with name:", name, "score:", score)
 	updateFunc := js.Global().Get("updateLeaderboard")
+	js.Global().Get("console").Call("log", "[GO] updateLeaderboard function found:", !updateFunc.IsUndefined() && !updateFunc.IsNull())
 	if !updateFunc.IsUndefined() && !updateFunc.IsNull() {
+		js.Global().Get("console").Call("log", "[GO] Invoking updateLeaderboard")
 		updateFunc.Invoke(name, score)
+		js.Global().Get("console").Call("log", "[GO] updateLeaderboard invoked successfully")
+	} else {
+		js.Global().Get("console").Call("log", "[GO] ERROR: updateLeaderboard is undefined or null")
 	}
 }
 
@@ -23,21 +29,30 @@ func (g *Game) showNameInputModal() {
 	}
 
 	callback := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		js.Global().Get("console").Call("log", "[GO] Modal callback executed, args length:", len(args))
 		if len(args) > 0 {
 			name := args[0].String()
+			js.Global().Get("console").Call("log", "[GO] Name received from modal:", name)
 			if name != "" {
 				g.playerName = name
 				g.leaderboard.AddScore(name, g.score)
+				js.Global().Get("console").Call("log", "[GO] Added score to local leaderboard")
 
 				data, err := g.leaderboard.ToJSON()
 				if err == nil {
 					g.storage.SaveLeaderboard(data)
+					js.Global().Get("console").Call("log", "[GO] Saved to local storage")
 				}
 
 				g.notifyWebLeaderboard(name, g.score)
+			} else {
+				js.Global().Get("console").Call("log", "[GO] Name is empty, skipping save")
 			}
+		} else {
+			js.Global().Get("console").Call("log", "[GO] No args received from modal")
 		}
 		g.state = config.StateGameOver
+		js.Global().Get("console").Call("log", "[GO] State set to GameOver")
 		return nil
 	})
 
