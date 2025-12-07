af1215763d9b41f7e66d6f8e7d819c52553afbe2
Luuan11 <luancraft55@gmail.com>
2025-12-07 18:38:48 -0300

    :sparkles: feat: add player death animation with continuous explosions

--- internal/config/constants.go
+++ internal/config/constants.go
@@ -18,10 +18,19 @@ const (
 	MeteorRotationMin = -0.02
 	MeteorRotationMax = 0.02
 
+	// Special Meteor Types
+	MeteorIceSlowFactor         = 0.5 // Player moves at 50% speed
+	MeteorIceSlowDuration       = 3 * time.Second
+	MeteorExplosiveRadius       = 80.0
+	MeteorExplosiveDamageRadius = 60.0
+	MeteorIceSpawnChance        = 0.08 // 8% chance
+	MeteorExplosiveSpawnChance  = 0.06 // 6% chance
+	MeteorExplosiveSpeed        = 3.5  // Slower than normal meteors
+
 	LaserSpeed      = 7.0
 	SuperLaserSpeed = 12.0
 
-	PowerUpSpeed     = 6.0
+	PowerUpSpeed     = 3.0
 	PowerUpSpawnTime = 20 * time.Second
 	SuperPowerTime   = 10 * time.Second
 	ShieldTime       = 10 * time.Second
@@ -30,6 +39,8 @@ const (
 
 	LaserBeamTime       = 3500 * time.Millisecond
 	NukeClearScreenTime = 5 * time.Second
+	MultiplierTime      = 20 * time.Second
+	MultiplierBonus     = 2.0
 	MinWaveForLaser     = 5
 	MinWaveForNuke      = 5
 
@@ -49,16 +60,20 @@ const (
 	ParticleCount    = 15
 	ParticleSpeed    = 3.0
 
+	PlayerDeathAnimationDuration = 90 // 1.5 seconds at 60 FPS
+	PlayerDeathExplosionCount    = 30 // Number of explosion particles
+
 	ScreenShakeDuration   = 10
 	ScreenShakeIntensity  = 8.0
 	ScreenShakeBossHit    = 5
 	ScreenShakeBossDefeat = 20
 
-	BossWaveInterval     = 5
-	BossScoreThreshold   = 250
-	BossReward           = 100
-	BossCooldownTime     = 60 * time.Second
-	PowerUpSpawnTimeBoss = 8 * time.Second
+	BossWaveInterval          = 5
+	BossScoreThreshold        = 250
+	BossReward                = 100
+	BossCooldownTime          = 60 * time.Second
+	PowerUpSpawnTimeBoss      = 8 * time.Second
+	PostBossInvincibilityTime = 3 * time.Second
 
 	BossTankHealth          = 150
 	BossTankSpeed           = 2.0
@@ -66,30 +81,51 @@ const (
 	BossTankProjectileSpeed = 4.0
 
 	BossSniperHealth          = 80
-	BossSniperSpeed           = 4.0
+	BossSniperSpeed           = 4.8
 	BossSniperShootCooldown   = time.Millisecond * 600
 	BossSniperProjectileSpeed = 7.0
 
 	BossSwarmHealth          = 100
-	BossSwarmSpeed           = 3.5
+	BossSwarmSpeed           = 5.25
 	BossSwarmShootCooldown   = time.Millisecond * 1200
 	BossSwarmProjectileSpeed = 5.0
-	BossMinionHealth         = 5
+	BossMinionHealth         = 8
 	BossMinionSize           = 15.0
-	BossMinionCount          = 3
+	BossMinionCount          = 2
+	BossSwarmMinionCount     = 3
+	PointsPerMinionKill      = 25
 
 	// UI Constants
 	PauseIconSize   = 30
 	PauseIconMargin = 15
 	ParticleSize    = 2.0
 
+	HeartSpacing         = 40
+	HeartOffsetX         = 10
+	HeartOffsetY         = 10
+	PowerUpBarStartY     = 100
+	PowerUpBarSpacing    = 30
+	JoystickOffsetX      = 100
+	JoystickOffsetY      = 120
+	JoystickRadius       = 60
+	ShootButtonOffsetX   = 100
+	ShootButtonOffsetY   = 120
+	ShootButtonRadius    = 50
+	BossAnnouncementFade = 30
+
 	// Game Mechanics
-	BossTypesCount        = 3
-	MeteorsPerWaveOffset  = 1
-	WaveMeteoIncrement    = 5
-	WaveDifficultyFactor  = 0.15
-	BossWarningShakeTime  = 30
-	BossScoreProximity    = 10
-	ExplosionParticlesMul = 3
-	MinionParticles       = 3
+	BossTypesCount             = 3
+	MeteorsPerWaveOffset       = 1
+	WaveMeteoIncrement         = 5
+	WaveDifficultyFactor       = 0.15
+	BossWarningShakeTime       = 30
+	BossScoreProximity         = 10
+	BossAnnouncementTime       = 120
+	ExplosionParticlesMul      = 3
+	MinionParticles            = 3
+	BossProjectileOffsetY      = 40
+	BossSwarmProjectileOffsetX = 30
+	BossShootMinY              = 100
+	InitialCapacityLasers      = 100
+	InitialCapacityParticles   = 100
 )
